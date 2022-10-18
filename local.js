9eac9d14dab3593574c92ecebb925c81796c9098
Torbjörn Petersen <kontakt@entepe.de>
2022-10-18 16:25:28 +0200

    Update local.js

--- local.js
+++ local.js
@@ -1,6 +1,5 @@
 !function(){
     if(window.location.host.indexOf("localhost")>-1||window.location.host.indexOf("127.0.0.1")>-1||window.location.host.match(/[A-Za-z0-9\-]+\.local/gi).length > 0){
-		console.log('GOT A MATCH')
         var div = document.createElement('div');
         div.setAttribute('class', 'corner-ribbon top-right s1 extension-sticky red shadow');
         div.innerText = window.location.host;
